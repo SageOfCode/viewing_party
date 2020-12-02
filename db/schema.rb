c35a14b77ddeecee3524fa40913ef74d3edafc42
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-02 14:00:46 -0700

    Remove username from User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_001658) do
+ActiveRecord::Schema.define(version: 2020_12_02_204854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
