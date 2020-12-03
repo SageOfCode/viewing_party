d77da4f91319dcd6fc1a55524ce18203a3740376
Grant Dempsey <CryptoDemps@gmail.com>
2020-12-02 19:48:16 -0700

    Add MovieApiService class

---
+++ app/service/movie_api_service.rb
@@ -0,0 +1,11 @@
+# class MovieApiService
+#
+#   def conn
+#     api = Faraday.new("https://api.themoviedb.org") do |f|
+#       f.headers["api_key"] = ENV["MOVIE_API_KEY"]
+#     end
+#     require "pry"
+#     binding.pry
+#   end
+#
+# end
