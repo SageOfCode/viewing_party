25e9ce9c869a65b8f9c5ddb22f2b26fa0be6133b
Grant Dempsey <CryptoDemps@gmail.com>
2020-12-04 16:35:48 -0700

    Add test for discover movies function

---
+++ spec/features/discover/user_can_search_for_movie.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "user can search movies through the find movies button" do
+  before :each do
+    user = User.create!(email: "grant@awesomeguy.com", password: "password", role: 1)
+
+    visit "/login"
+
+    fill_in :email, with: "grant@awesomeguy.com"
+    fill_in :password, with: "password"
+
+    click_button "Login"
+  end
+
+  it "user can discover top 40 movies and see results on page" do
+
+    expect(current_path).to eq("/dashboard")
+
+    click_button "Discover Movies"
+
+    expect(current_path).to eq("/discover")
+
+    click_button "Find Top Rated Movies"
+    save_and_open_page
+
+    expect(page).to have_css('.movies')
+    movies = find('.movies').text
+    expect(movies).to_not be_empty
+  end
+end
