09d25d8f86298ee44fbd67bde55a26e2908612f0
Jose Lopez <joselopez11394@gmail.com>
2020-12-06 19:20:41 -0500

    adds test to api calls and methods

--- spec/models/movie_api_test.rb
+++ spec/models/movie_api_test.rb
@@ -9,5 +9,19 @@ RSpec.describe MovieApiService do
         expect(movies.count).to eq(40)
       end
     end
+
+    it "top_rated_movies" do
+      VCR.use_cassette("top_rated") do
+        movies = MovieApiService.top_rated_movies
+        expect(movies).to be_an(Array)
+        expect(movies.count).to eq(40)
+      end
+    end
+
+    it "movie_details" do
+      VCR.use_cassette("movie_details") do
+        MovieApiService.movie_details(372058)
+      end
+    end
   end
 end
