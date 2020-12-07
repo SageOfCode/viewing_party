d8f1a3288be50c719e6d0d5abe5bdbd12ee60ed5
Jose Lopez <joselopez11394@gmail.com>
2020-12-06 19:21:30 -0500

    adds api methods and creates class for movie details to simplify using information

--- app/service/movie_api_service.rb
+++ app/service/movie_api_service.rb
@@ -2,6 +2,27 @@ require "faraday"
 
 class MovieApiService
 
+  def self.movie_details(movie_id)
+    response = conn.get("/3/movie/#{movie_id}") do |req|
+      req.params[:append_to_response] = "credits,reviews"
+    end
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    MovieObject.new(json_response)
+  end
+
+  def self.top_rated_movies
+    page = 1
+    movies = []
+    2.times do
+      response = conn.get("/3/movie/top_rated") do |req|
+        req.params[:page] = page
+      end
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      movies << json_response[:results]
+      page += 1
+    end
+    movies.flatten!
+  end
 
   def self.movies(query)
     page = 1
@@ -18,7 +39,7 @@ class MovieApiService
   end
 
   private
-  
+
   def self.conn
     Faraday.new("https://api.themoviedb.org") do |f|
       f.params["api_key"] = ENV["MOVIE_API_KEY"]
