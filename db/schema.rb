80645e2ff1a716e08439118db94887534d97008a
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-07 20:46:49 -0700

    Co-authored-by: Jose Lopez <joselopez11394@gmail.com> Co-authored-by: Grant Dempsey <CryptoDemps@gmail.com> Added joins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_222128) do
+ActiveRecord::Schema.define(version: 2020_12_08_002722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_1"
+    t.integer "user_2"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
