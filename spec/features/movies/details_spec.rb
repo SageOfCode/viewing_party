6df4fe84a2e69567107e0c6bb449a1313af4c786
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-09 13:45:04 -0700

    Replace stub request with VCR cassette

--- spec/features/movies/details_spec.rb
+++ spec/features/movies/details_spec.rb
@@ -3,34 +3,29 @@ require 'rails_helper'
 RSpec.describe "As an authenticated user" do
   describe "When I visit the movie's detail page" do
     it 'I should see a movies info' do
-      json_response = File.read("spec/fixtures/movie_search.json")
+      VCR.use_cassette('movie_details') do 
+        user = User.create!(username: "Grant", email: "grant@awesomeguy.com", password: "password")
 
-      stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated").
-         with(
-           headers: {
-          'X-Api-Key'=>ENV["MOVIE_API_KEY"]
-           }).
-      to_return(status: 200, body: json_response, headers: {})
+        visit "/login"
 
-      user = User.create!(username: "Grant", email: "grant@awesomeguy.com", password: "password")
+        fill_in :email, with: "grant@awesomeguy.com"
+        fill_in :password, with: "password"
 
-      visit "/login"
+        click_button "Login"
 
-      fill_in :email, with: "grant@awesomeguy.com"
-      fill_in :password, with: "password"
+        visit "/"
+        click_button "Discover Movies"
+        
+        click_button "Find Top Rated Movies"
+        
+        fill_in "movie", with: "Historias lamentables"
+        click_button "Find Movies"
 
-      click_button "Login"
+        click_link "Historias lamentables"
 
-      visit "/"
-      click_button "Discover Movies"
-      
-      click_button "Find Top Rated Movies"
-      save_and_open_page
-
-      click_link "Vampire Virus"
-
-      expect(current_path).to eq("/movies/#{movie[1][:id]}")
-      expect(page).to have_content("Vampire Virus")
+        expect(current_path).to eq("/movies/650747")
+        expect(page).to have_content("Historias lamentables")
+      end 
     end
   end
 end 
