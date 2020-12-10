93420c1f2a9f20690d1703c2a89ea190bebec5d3
Jose Lopez <joselopez11394@gmail.com>
2020-12-10 09:21:41 -0500

    Removed unneccessary relationships to movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,4 @@
 class Movie < ApplicationRecord
   validates_presence_of :title, :api_id, :duration, require: true
   has_many :movie_parties
-  has_many :party_guests
-  has_many :users, through: :party_guests
 end
