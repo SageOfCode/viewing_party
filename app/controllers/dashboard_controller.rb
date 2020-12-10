510b5bcfa2b020f3e675c52945536654f493e1d6
Sage Gonzales <sagegonzales15@gmail.com>
2020-12-09 20:40:43 -0700

    Update dashboard to include viewing party info

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    # require 'pry'; binding.pry
     @friends = current_user.friends
+    @movie_parties = current_user.movie_parties
   end
 end
